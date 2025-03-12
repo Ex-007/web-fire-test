542defdd5494dc09c5d2cfb3e60e37bc232a4a15
agbebibidemi02 <agbebibidemi02@gmail.com>
2025-03-12 11:22:29 +0000

    added more changes plus installing pinia and firebase

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Is This is the first page of trying out google's editor on the internet</h1>
+    </div>
+</template>
+
+<style scoped>
+    body{
+        background-color: red;
+        color: white;
+    }
+
+    h1{
+        color: red;
+    }
+</style>
